2ba6cfa1d3dcf000dc07b59e401ddb1573c7a0d6
Anna Laves <anna.laves@web.de>
2026-02-15 14:39:47 +0100

    [grundlagen] replaced curved carousel, replaced orbit animation, fixed mask

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,8 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
-};
+	/* config options here */
+	reactCompiler: true,
+	images: {
+		domains: ['picsum.photos'],
+	},
+}
 
-export default nextConfig;
+export default nextConfig
--- web/src/app/grundlagen/page.tsx
+++ web/src/app/grundlagen/page.tsx
@@ -1,6 +1,7 @@
-import ScrollCarousel from '@/components/custom/ScrollCarousel'
-
-import OrbitImages from '../../components/OrbitImages'
+import Silk from '../../components/Silk'
+import Carousel from '@/components/custom/Carousel'
+import { Suspense } from 'react'
+import Image from 'next/image'
 
 export default function Page() {
 	const images = [
@@ -12,36 +13,45 @@ export default function Page() {
 		'https://picsum.photos/300/300?grayscale&random=6',
 	]
 	return (
-		<div className='h-screen lg:h-fit w-full wrapper-cols-12 bg-[var(--color-granite)]'>
-			<h2 className='h-10 mt-[3em] text-center col-span-12'>Grundlagen</h2>
-			<ScrollCarousel
-				items={[
-					<div>Item 1</div>,
-					<div>Item 2</div>,
-					<div>Item 3</div>,
-					<div>Item 4</div>,
-					<div>Item 5</div>,
-					<div>Item 6</div>,
-					// ...
-				]}
-				className='hidden lg:grid col-span-12 overflow-hidden! z-10 -mx-20'
+		<div className='overflow-auto min-h-screen lg:max-h-screen fixed w-full wrapper-cols-12 bg-[var(--color-glossyBlack)]'>
+			<Silk
+				speed={5}
+				scale={1}
+				color='#F3EFE3'
+				noiseIntensity={1.5}
+				rotation={0}
+				className='lg:hidden'
 			/>
-			<OrbitImages
-				images={images}
-				shape='ellipse'
-				radiusX={500}
-				radiusY={700}
-				rotation={-8}
-				duration={30}
-				itemSize={300}
-				responsive={true}
-				radius={200}
-				direction='normal'
-				fill
-				showPath
-				paused={false}
-				className='col-span-12 h-full lg:hidden '
-			/>
+			{/* phone: titel fest und bilder scrollen 'untendrunter durch' */}
+			<h2 className='w-full fixed lg:relative z-10! h-fit mt-[0.8em] text-center col-span-12 text-[var(--color-softLinen)]'>
+				Grundlagen
+			</h2>
+			<Suspense fallback={<div>Loading...</div>}>
+				<Carousel className='hidden lg:flex col-span-12 -mx-20 height-[100vh]' />
+			</Suspense>
+
+			<div className='lg:hidden absolute z-1! top-60 col-span-12 min-h-screen h-fit w-full z-10'>
+				<div className='flex flex-col gap-4'>
+					{images.map((src, index) => (
+						<div
+							key={index}
+							className='relative max-w-screen text-center'
+						>
+							<Image
+								src={src}
+								alt={`Image ${index + 1}`}
+								className='h-[15vh] w-screen object-cover'
+								width={100}
+								height={100}
+								quality={100}
+							/>
+							<span className='text-white absolute top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2'>
+								Thema
+							</span>
+						</div>
+					))}
+				</div>
+			</div>
 		</div>
 	)
 }
