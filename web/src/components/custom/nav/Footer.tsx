bbe5b9fedf42197d9127ca4d5d01646645744385
Anna Laves <anna.laves@web.de>
2026-03-12 12:06:08 +0100

    [adjustments] fixed footer

--- web/src/components/custom/nav/Footer.tsx
+++ web/src/components/custom/nav/Footer.tsx
@@ -3,77 +3,79 @@ import { navQuery } from '@/lib/sanity/queries'
 import Link from 'next/link'
 import { TextLink } from '../Link'
 import type { NavLink } from './types'
-import MobileNav from './MobileNav'
 import { urlForImage } from '@/lib/sanity/utils'
 import { SanityImage } from '@/components/SanityImage'
 
 export default async function Footer() {
 	const nav = await client.fetch(navQuery)
-	const footerImgAssets = nav.footerlogo
-		? urlForImage(nav.footerlogo.asset)
-		: urlForImage(nav.navlogo.asset) // Fallback auf Nav-Logo
+
+	const logo = nav.footerlogo ?? nav.navlogo
+	const footerImgAssets = urlForImage(logo.asset)
+
+	const logoWidth = logo?.asset?.metadata?.dimensions?.width ?? 240
+	const logoHeight = logo?.asset?.metadata?.dimensions?.height ?? 80
 
 	return (
-		<>
-			<footer
-				className='bg-(--color-glossyBlack) flex flex-col md:flex-row justify-between p-3'
-				aria-label='Fußzeile'
-			>
+		<footer
+			className='bg-(--color-glossyBlack) p-3 md:flex md:flex-row md:items-start md:justify-between md:gap-6'
+			aria-label='Fußzeile'
+		>
+			<div className='grid grid-cols-2 md:contents'>
 				<Link
-					href={nav.navlogolink} // TODO: besser lösung für link? damit
+					href={nav.navlogolink}
 					aria-label='Zur Startseite'
-					className='h-full max-w-[30vw] w-full lg:w-fit aspect-square p-5'
+					className='col-span-1 shrink-0 w-30 sm:w-35 md:w-47.5'
 				>
 					<SanityImage
 						src={footerImgAssets}
-						className='object-contain relative!'
-						fill
-						alt={nav.footerlogo?.alt ?? 'ki360 Logo'}
+						alt={logo?.alt ?? 'ki360 Logo'}
+						width={logoWidth}
+						height={logoHeight}
+						className='w-full h-auto object-contain'
 					/>
 				</Link>
 
 				<ul
-					className='max-w-screen-xl w-full px-4 sm:px-6  text-gray-800 flex flex-wrap  justify-center lg:justify-between'
+					className='col-span-1 flex flex-col text-right max-w-8xl md:w-full md:px-4 md:flex-wrap md:flex-row md:justify-between md:text-left'
 					role='list'
 				>
-					{nav.items.map((link: NavLink) => {
-						return (
-							<li
-								key={link.text}
-								className='p-5'
-							>
-								<span className='text-[var(--color-frost)] font-bold'>
+					{nav.items.map((link: NavLink) => (
+						<li
+							key={link.text}
+							className='pb-3 md:p-5 min-w-45'
+						>
+							{link.url ? (
+								<TextLink
+									className='block text-(--color-frost) font-bold hover:text-(--color-granite) focus:underline-2 focus:underline-offset-2'
+									href={link.url}
+									text={link.text}
+								/>
+							) : (
+								<span className='block text-(--color-frost) font-bold'>
 									{link.text}
 								</span>
-								<TextLink
-									className={`my-3 block hover:text-[var(--color-granite)] focus:underline-2 focus:underline-offset-2 text-[var(--color-frost)]`}
-									href={link.url}
-									text={link.text}
-								/>
-								<TextLink
-									className={`my-3 block hover:text-[var(--color-granite)] focus:underline-2 focus:underline-offset-2 text-[var(--color-frost)]`}
-									href={link.url}
-									text={link.text}
-								/>
-								<TextLink
-									className={`my-3 block hover:text-[var(--color-granite)] focus:underline-2 focus:underline-offset-2 text-[var(--color-frost)]`}
-									href={link.url}
-									text={link.text}
-								/>
-								<TextLink
-									className={`my-3 block hover:text-[var(--color-granite)] focus:underline-2 focus:underline-offset-2 text-[var(--color-frost)]`}
-									href={link.url}
-									text={link.text}
-								/>
-							</li>
-						)
-					})}
+							)}
+
+							{link.subitems?.length ? (
+								<ul
+									className='md:mt-3 space-y-2'
+									role='list'
+								>
+									{link.subitems.map((subitem) => (
+										<li key={`${link.text}-${subitem.text}`}>
+											<TextLink
+												className='block text-(--color-frost) hover:text-(--color-granite) focus:underline-2 focus:underline-offset-2'
+												href={subitem.url}
+												text={subitem.text}
+											/>
+										</li>
+									))}
+								</ul>
+							) : null}
+						</li>
+					))}
 				</ul>
-				{/* <MobileNav
-					links={nav.items}
-					className='flex md:hidden'
-				/> */}
-			</footer>
-		</>
+			</div>
+		</footer>
 	)
 }
--- web/src/components/custom/nav/types.ts
+++ web/src/components/custom/nav/types.ts
@@ -1,10 +1,17 @@
-interface NavLink {
+type NavSubItem = {
 	text: string
 	url: string
 }
 
+type NavLink = {
+	text: string
+	url?: string
+	subitems?: NavSubItem[]
+}
+
+// TODO: interface / type?
 interface MobileNavProps {
 	links: NavLink[]
 	className?: string
 }
-export type { NavLink, MobileNavProps }
+export type { NavSubItem, NavLink, MobileNavProps }
